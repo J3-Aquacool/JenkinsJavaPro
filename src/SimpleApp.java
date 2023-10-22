da8346f2ea26da258429a7c0b7468eec1ba10f33
J3-Aquacool <jagdish.java2@gmail.com>
2023-10-22 13:44:28 +0530

    ADDED FILE 1

---
+++ src/SimpleApp.java
@@ -0,0 +1,17 @@
+
+
+public class SimpleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("App Started...............");
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("My Count is:"+i);
+			
+		}
+	}
+
+}
